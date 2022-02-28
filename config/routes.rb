f43091478ba14a58e522ec99745bdb38fcaee3df
Quynh Anh Pham <qap.pham@gmail.com>
2022-02-28 10:55:15 +0100

    Adding form to create a list and the show of a list

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
+  get 'lists/new', to: 'lists#new'
+  get 'lists/:id', to: 'lists#show', as: :list
+  post 'lists', to: 'lists#create'
 end
