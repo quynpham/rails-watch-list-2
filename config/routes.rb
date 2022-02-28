571dea9aaeb96f187c7a5370776c2b239a024ea5
Quynh Anh Pham <qap.pham@gmail.com>
2022-02-28 11:29:57 +0100

    Bookmarks show created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
-  get 'lists/new', to: 'lists#new'
-  get 'lists/:id', to: 'lists#show', as: :list
-  post 'lists', to: 'lists#create'
+  resources :lists, only: [:index, :new, :show, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
