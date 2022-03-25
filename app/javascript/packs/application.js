9a60cdaa7707e91a6568d56ed83152575ec5b786
Quynh Anh Pham <qap.pham@gmail.com>
2022-03-25 16:06:12 +0100

    Configuring tailwind for prod

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,13 +8,12 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap";
-import "tailwindcss";
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import "stylesheets/application";
+import "../css/application.css";
 import "@fortawesome/fontawesome-free/js/all";
 
 import "controllers"
